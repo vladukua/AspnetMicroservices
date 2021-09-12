59898acf547ddc7a730670148d042e5d80d5bb76
Volodymyr Teodorovych <vladukteo@gmail.com>
2021-09-12 13:21:00 +0300

    Added CatalogContext.

---
+++ src/Services/Catalog/Catalog.Api/Data/ICatalogContext.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
