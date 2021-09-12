18f8a161ea0dbb59060c6352378bdd4a0433d256
Volodymyr Teodorovych <vladukteo@gmail.com>
2021-09-12 13:36:56 +0300

    Added product repository.

---
+++ src/Services/Catalog/Catalog.Api/Repositories/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.API.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+
+        Task<Product> GetProductByIdAsync(string id);
+
+        Task<IEnumerable<Product>> GetProductsByNameAsync(string name);
+        
+        Task<IEnumerable<Product>> GetProductsByCategoryAsync(string categodyName);
+
+        Task CreateProductAsync(Product product);
+
+        Task<bool> UpdateProductAsync(Product product);
+
+        Task<bool> DeleteProductAsync(string id);
+    }
+}
